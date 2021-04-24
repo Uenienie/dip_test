c45fdc06ff3d47d2b09e14cf4919ad39696befdd
Ueki <71415579+Uenienie@users.noreply.github.com>
2021-04-24 18:40:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template, request, jsonify
 import pandas as pd
 import warnings
+warnings.simplefilter("ignore")
 import app_content.implementation as imp
 
 #Flaskオブジェクトの生成
